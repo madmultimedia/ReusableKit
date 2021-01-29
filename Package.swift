e204a8d4d9d98697ae822b363c871d9e6c5178b5
Daniel van den Brink <daniel@madmultimedia.nl>
2021-01-29 15:51:11 +0100

    Use RxSwift 6.0

--- Package.swift
+++ Package.swift
@@ -1,24 +1,27 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
   name: "ReusableKit",
   platforms: [
-    .iOS(.v8), .tvOS(.v9)
+    .iOS(.v9), .tvOS(.v9)
   ],
   products: [
     .library(name: "ReusableKit", targets: ["ReusableKit"]),
     .library(name: "RxReusableKit", targets: ["RxReusableKit"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0")),
+    .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
   ],
   targets: [
     .target(name: "ReusableKit"),
-    .target(name: "RxReusableKit", dependencies: ["ReusableKit", "RxSwift", "RxCocoa"]),
+    .target(name: "RxReusableKit", dependencies: [
+        "ReusableKit",
+        "RxSwift",
+        .product(name: "RxCocoa", package: "RxSwift"),
+    ]),
     .testTarget(name: "ReusableKitTests", dependencies: ["ReusableKit"]),
     .testTarget(name: "RxReusableKitTests", dependencies: ["RxReusableKit"]),
-  ],
-  swiftLanguageVersions: [.v5]
+  ]
 )
